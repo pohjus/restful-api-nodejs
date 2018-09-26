800f9039699f89276760ae9d862c43db6207b53d
pohjus <jussi.pohjolainen@gmail.com>
2018-09-26 13:35:56 +0300

    More cors

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const locationsmemory = require('./routes/locations.js')
 
 const app = express()
 app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Expose-Headers", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
 app.use(express.json())
 app.use('/locations', locationsmemory)
 
